cc5084d11586f9f35ca0ef057fa571f10674888b
DavidTr <davidrozotuberquia@gmail.com>
2023-02-03 22:45:01 -0500

    Se agrega un loader

--- ASSETS/JS/app.js
+++ ASSETS/JS/app.js
@@ -3,3 +3,9 @@ hamburger.onclick = function() {
     nav = document.querySelector(".nav");
     nav.classList.toggle("active");
 }
+
+window.onload = function () {
+    let contenedor = document.getElementById('cargador');
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+  }
